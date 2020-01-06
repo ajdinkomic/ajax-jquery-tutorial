4ac67a8257cd0d798b59226aa33aba854e27e894
Ajdin Komic <ajdin.komic12@gmail.com>
2020-01-06 19:08:41 +0100

    add ajax feature

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require("express"),
-app     = express(),
-mongoose = require("mongoose"),
-bodyParser = require("body-parser"),
-expressSanitizer = require("express-sanitizer"),
-methodOverride = require('method-override');
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  expressSanitizer = require("express-sanitizer"),
+  methodOverride = require('method-override');
 
 mongoose.connect("mongodb://localhost/todo_app");
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(expressSanitizer());
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
@@ -18,74 +20,100 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.redirect("/todos");
 });
 
-app.get("/todos", function(req, res){
-  Todo.find({}, function(err, todos){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("index", {todos: todos}); 
-    }
-  })
+// function to be used in the .get("/todos", ..) route
+// this allows us to escape any special characters with a backslash
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+app.get("/todos", function (req, res) {
+  if (req.query.keyword) { // if there's a query string called keyword then..
+    // set the constant (variable) regex equal to a new regular expression created from the keyword 
+    // that we pulled from the query string
+    var regex = new RegExp(escapeRegex(req.query.keyword), 'gi');
+    // query the database for Todos with text property that match the regular expression version of the search keyword
+    Todo.find({
+      text: regex
+    }, function (err, todos) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(todos);
+      }
+    });
+  } else {
+    // if there wasn't any query string keyword then..
+    Todo.find({}, function (err, todos) { // query the db for all todos
+      if (err) {
+        console.log(err);
+      } else {
+        if (req.xhr) { // if request was made with AJAX then ..
+          res.json(todos); // send back all todos as JSON
+        } else {
+          res.render("index", {
+            todos: todos
+          }); // otherwise render the index view and pass in all todos with EJS
+        }
+      }
+    });
+  }
 });
 
-app.get("/todos/new", function(req, res){
- res.render("new"); 
+app.get("/todos/new", function (req, res) {
+  res.render("new");
 });
 
-app.post("/todos", function(req, res){
- req.body.todo.text = req.sanitize(req.body.todo.text);
- var formData = req.body.todo;
- Todo.create(formData, function(err, newTodo){
-    if(err){
+app.post("/todos", function (req, res) {
+  req.body.todo.text = req.sanitize(req.body.todo.text); // prevent script injection
+  var formData = req.body.todo;
+  Todo.create(formData, function (err, newTodo) {
+    if (err) {
       res.render("new");
     } else {
-        res.redirect("/todos");
+      res.json(newTodo);
     }
   });
 });
 
-app.get("/todos/:id/edit", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-     res.redirect("/")
-   } else {
-      res.render("edit", {todo: todo});
-   }
- });
+app.get("/todos/:id/edit", function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+      res.redirect("/")
+    } else {
+      res.render("edit", {
+        todo: todo
+      });
+    }
+  });
 });
 
-app.put("/todos/:id", function(req, res){
- Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      res.redirect('/');
-   }
- });
+app.put("/todos/:id", function (req, res) {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, {
+    new: true
+  }, function (err, todo) { // new:true reutrns updated item, not original
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
 });
 
-app.delete("/todos/:id", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      todo.remove();
-      res.redirect("/todos");
-   }
- }); 
+app.delete("/todos/:id", function (req, res) {
+  Todo.findByIdAndRemove(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
 });
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Server running on port 3000');
 });
-
-// // Uncomment the three lines of code below and comment out or remove lines 84 - 86 if using cloud9
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("The server has started!");
-// });
